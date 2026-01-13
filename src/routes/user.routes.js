63b799c3759e558071c4917bee3bd031e9907be6
Vaibhav Bhagat <vaibhavbhagat123455@gmail.com>
2026-01-13 20:08:08 +0530

    Delete src directory

--- src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { updateFcmToken } from '../controllers/user.controller.js';
-
-const router = express.Router();
-
-// Route: PATCH /api/users/:id/fcm-token
-router.patch('/:id/fcm-token', updateFcmToken);
-
-export default router;
